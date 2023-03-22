8cf3c36ce72c87cec71b838813300de60e25f0df
Che-tsun Ho <republicho@foxmail.com>
2023-03-22 10:14:22 +0800

    update security features

--- src/main/java/com/g31/demo/model/User.java
+++ src/main/java/com/g31/demo/model/User.java
@@ -1,20 +1,25 @@
 package com.g31.demo.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 /**
  * @Description:
  */
-
-
 @Table(name = "user")
 @Entity
-public class User {
+public class User implements UserDetails {
     @SequenceGenerator(
             name = "users_sequence",
             sequenceName = "users_sequence",
@@ -35,6 +40,26 @@ public class User {
     @Length(min = 8, message = "Password should be at least 8 characters long")
     @Column(name = "password")
     private String password;
+
+    @NotNull(message = "Email cannot be empty")
+    @Email(message = "Please enter a valid email address")
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "locked")
+    private Boolean locked = false;
+
+    @Column(name = "enabled")
+    private Boolean enabled = true;
+
     @OneToMany
     private List<User> friendList;
 
@@ -59,8 +84,33 @@ public class User {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
     public String getPassword() {
