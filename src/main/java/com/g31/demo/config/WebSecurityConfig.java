f6f07f90c79985d29d596430e64e604ce7a94d65
Che-tsun Ho <republicho@foxmail.com>
2023-03-22 15:13:48 +0800

    update

---
+++ src/main/java/com/g31/demo/config/WebSecurityConfig.java
@@ -0,0 +1,91 @@
+package com.g31.demo.config;
+
+import com.g31.demo.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/**
+ * @Description: 这里的内容还需要改一改 因为spring最新版本是6
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private LoginSuccessHandler successHandler;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http.authorizeRequests()
+//                // URL matching for accessibility
+//                .antMatchers("/", "/login", "/register").permitAll()
+//                .antMatchers("/admin/**").hasAnyAuthority("ADMIN")
+//                .antMatchers("/account/**").hasAnyAuthority("USER")
+//                .anyRequest().authenticated()
+//                .and()
+//                // form login
+//                .csrf().disable().formLogin()
+//                .loginPage("/login")
+//                .failureUrl("/login?error=true")
+//                .successHandler(successHandler)
+//                .usernameParameter("email")
+//                .passwordParameter("password")
+//                .and()
+//                // logout
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/")
+//                .and()
+//                .exceptionHandling()
+//                .accessDeniedPage("/access-denied");
+//
+//        http.authenticationProvider(authenticationProvider());
+//        http.headers().frameOptions().sameOrigin();
+//
+//        return http.build();
+//    }
+
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+//    }
+
+}
