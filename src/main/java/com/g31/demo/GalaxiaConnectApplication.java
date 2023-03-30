f81833ecc565b7edade118048b29a9b2dc45fdb9
Che-tsun Ho <republicho@foxmail.com>
2023-03-30 08:56:28 +0800

    Update GalaxiaConnectApplication.java

--- src/main/java/com/g31/demo/GalaxiaConnectApplication.java
+++ src/main/java/com/g31/demo/GalaxiaConnectApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 @ComponentScan(basePackages = {"com.g31.demo.repository"})
 @EnableJpaRepositories("com.g31.demo.repository")
 public class GalaxiaConnectApplication implements CommandLineRunner {
